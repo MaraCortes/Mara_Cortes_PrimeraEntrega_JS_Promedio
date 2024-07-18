514a849fd00b6434d13ca6948b37b5cfd93be049
Mara Cortes <cortesmara25@gmail.com>
2024-07-18 15:29:35 -0300

    Primer commit de Promedio

---
+++ JS/main.js
@@ -0,0 +1,54 @@
+function ingresarNotas() {
+    let nombre = prompt("Ingrese el nombre del alumno");
+
+    while (!/^[a-zA-Z]+$/.test(nombre)) {
+        nombre = prompt("El nombre ingresado no es válido, por favor intente nuevamente");
+    }
+
+    let apellido = prompt("Ingrese el apellido del alumno");
+
+    while (!/^[a-zA-Z]+$/.test(apellido)) {
+        apellido = prompt("El apellido ingresado no es válido, por favor intente nuevamente");
+    }
+
+    let cantidadNotas = parseInt(prompt("Ingrese la cantidad de notas:"));
+
+
+    while (isNaN(cantidadNotas) || cantidadNotas <= 0) {
+        cantidadNotas = parseInt(prompt("Ingrese un número válido mayor que cero para la cantidad de notas:"));
+    }
+
+    let sumaNotas = 0;
+
+   
+    for (let x = 1; x <= cantidadNotas; x++) {
+        
+        let nota = parseFloat(prompt("Imgrese la nota " + x + " de " + nombre + " " + apellido ));
+
+
+        while (isNaN(nota) || nota < 0 || nota > 10) {
+            // nota = parseFloat(prompt(`Ingrese una nota válida entre 0 y 10 para la nota ${i} de ${nombre} ${apellido}:`));
+            nota = parseFloat(prompt("Ingrese una nota válida entre 0 y 10 para la nota " + x + " de " + nombre +  " " + apellido));
+        }
+
+        sumaNotas += nota;
+    }
+
+    // Calcular el promedio
+    let promedio = sumaNotas / cantidadNotas;
+
+    // Mostrar el resultado
+    alert("El promedio de " + nombre + " " + apellido + " es " + promedio.toFixed(2));
+
+    if (promedio >= 6) {
+        alert("El alumno está aprobado");
+    } else {
+        alert("El alumno está desaprobado");
+    }
+
+    ingresarNotas()
+}
+
+// Llamar a la función para iniciar el proceso de ingreso de notas
+ingresarNotas();
+
